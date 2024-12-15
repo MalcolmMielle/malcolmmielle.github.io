f7ef4f2479ad85f6cb5c8886b9997eb99003214e
Malcolm Mielle <malcolm.mielle@protonmail.com>
2024-12-15 02:08:28 +0100

    Quartz sync: Dec 15, 2024, 2:08 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -18,14 +18,15 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.Breadcrumbs(),
+    Component.Breadcrumbs({ spacerSymbol: "⇒" }),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
   ],
   left: [
     Component.PageTitle(),
-    Component.MobileOnly(Component.Darkmode()),
+    Component.DesktopOnly(Component.Search()),
+    Component.Darkmode(),
     Component.MobileOnly(Component.Spacer()),
     Component.DesktopOnly(Component.Explorer({
       folderDefaultState: "collapsed",
@@ -38,8 +39,6 @@ export const defaultContentPageLayout: PageLayout = {
     )),
   ],
   right: [
-    Component.DesktopOnly(Component.Darkmode()),
-    Component.DesktopOnly(Component.Search()),
     Component.DesktopOnly(Component.Graph()),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.MobileOnly(Component.Explorer({
@@ -57,10 +56,14 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [
+    Component.Breadcrumbs({ spacerSymbol: "⇒" }),
+    Component.ArticleTitle(),
+    Component.ContentMeta()],
   left: [
     Component.PageTitle(),
-    Component.MobileOnly(Component.Darkmode()),
+    Component.DesktopOnly(Component.Search()),
+    Component.Darkmode(),
     Component.MobileOnly(Component.Spacer()),
     Component.DesktopOnly(Component.Explorer({
       folderDefaultState: "collapsed",
@@ -73,8 +76,6 @@ export const defaultListPageLayout: PageLayout = {
     )),
   ],
   right: [
-    Component.DesktopOnly(Component.Darkmode()),
-    Component.DesktopOnly(Component.Search()),
     Component.DesktopOnly(Component.Graph()),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.MobileOnly(Component.Explorer({
